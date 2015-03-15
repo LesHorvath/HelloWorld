da4e12c03466f6c306fbda6da806151f0eed60e0
LesHorvath <les.horvath@mail.com>
2015-03-16 07:11:59 +1100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.awt.*;
+import java.sql.Time;
+
+public class Main {
+    static int a = 1;
+    static int b = 2;
+    public static void main(String[] args) {
+	System.out.println("Hello how are you? ");// write your code here
+    }
+}
